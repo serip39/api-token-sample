4f6a746487cc998b20b5d035881ad2e1f14e99ec
serip39 <serip39.yuki@gmail.com>
2021-07-09 21:36:23 +0900

    set rails-api-mode

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -24,6 +24,11 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
